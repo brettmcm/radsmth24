fd89c2e378db0513683f18819860c7b95d8e20b0
Brett McMillin <brettmcm@me.com>
2024-03-17 17:43:22 -0700

    services list

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,11 +5,62 @@ export default function Services(props: any) {
 
     return (
         <main className={styles.mainCol}>
-            <section className={about.main}>
+            <section className={about.overview}>
 
                 <p><b>McMillin Design Co.</b> is a graphic design studio specializing in brand strategy, visual identity, print and digital design. With over a decade of professional experience under our belts, we have honed our craft both at creative agencies and in-house at renowned brands.</p>
                 <p>Following the fun. Commited to the craft.</p>
 
+            </section>
+
+            <section className={about.services}>
+                <h2>Services</h2>
+
+                <div className={about.category}>
+                    <h3>Branding</h3>
+                    <div className={about.indexline}></div>
+                    <ul>
+                        <li>Strategy</li>
+                        <li>Logo design</li>
+                        <li>Style guide</li>
+                        <li>Brand guide</li>
+                        <li>Art direction</li>
+                    </ul>
+                </div>
+
+                <div className={about.category}>
+                    <h3>Graphic Design</h3>
+                    <div className={about.indexline}></div>
+                    <ul>
+                        <li>Posters</li>
+                        <li>Apparel</li>
+                        <li>Packaging</li>
+                        <li>Signage</li>
+                        <li>Stickers</li>
+                        <li>Illustration</li>
+                        <li>Lettering</li>
+                    </ul>
+                </div>
+
+                <div className={about.category}>
+                    <h3>Editorial Design</h3>
+                    <div className={about.indexline}></div>
+                    <ul>
+                        <li>Book design</li>
+                        <li>Layout</li>
+                        <li>Typesetting</li>
+                        <li>Print prep</li>
+                        <li>Press check</li>
+                    </ul>
+                </div>
+
+                <div className={about.category}>
+                    <h3>Interactive Design</h3>
+                    <div className={about.indexline}></div>
+                    <ul>
+                        <li>Web design</li>
+                        <li>Product design</li>
+                    </ul>
+                </div>
             </section>
         </main>
     )
