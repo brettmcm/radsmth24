e8e918e9916a2b27ec49bcd4fcc277f7fea7c8b2
Brett McMillin <brettmcm@me.com>
2024-04-04 19:24:48 -0700

    fixed footer maybe

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -4,19 +4,38 @@ import styles from './styles/nav.module.scss'
 
 import Image from 'next/image';
 
-import React, { useRef } from "react";
-import { motion, useScroll, useTransform } from "framer-motion";
+import React, { useRef, useEffect, useState } from "react";
+import { motion, motionValue, useScroll, useTransform } from "framer-motion";
 import Link from 'next/link';
 
 export default function Footer() {
 
-    const ref = useRef(null)
-    const { scrollYProgress } = useScroll({})
+    const ref = useRef(null);
+    const { scrollY } = useScroll();
+    const scrollPositionFromBottom = motionValue(0); // Initialized as a motionValue
+
+    useEffect(() => {
+        const calculateScrollPosition = () => {
+            const scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+            const positionFromBottom = scrollHeight - scrollY.get();
+            scrollPositionFromBottom.set(positionFromBottom); // Update the motionValue directly
+        };
+
+        calculateScrollPosition();
+        const unsubscribe = scrollY.onChange(calculateScrollPosition);
+
+        return () => {
+            unsubscribe();
+        };
+    }, [scrollY, scrollPositionFromBottom]);
+    
     const opacity = useTransform(
-        scrollYProgress,
-        [0, 0.75, 1],
-        [0, 0, 1],
-    )
+        scrollPositionFromBottom,
+        [0, 150],
+        [1, 0]
+    );
+
+
 
     return (
         <motion.footer
